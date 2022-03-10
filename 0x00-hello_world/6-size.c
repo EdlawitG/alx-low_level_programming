d6b58e6fc5773002264f29e84610c285b0639ef2
EdlawitG <eduedil30@gmail.com>
2022-03-10 06:17:37 -0800

    size commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+printf("Size of a char: %ld bytes(s)\n", sizeof(char));
+printf("Size of a int: %ld bytes(s)\n", sizeof(int));                
+printf("Size of a long int: %ld bytes(s)\n", sizeof(long int));        pringf("Size of a long long int: %ld bytes(s)\n", sizeof(long long int));
+printf("Size of a float: %ld byte(s)\n", sizeof(float));
+return (0);
+
+
+}
