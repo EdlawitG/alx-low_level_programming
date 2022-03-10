341968378b5d8cdfc7f134a2cca1a1af56d46a67
EdlawitG <eduedil30@gmail.com>
2022-03-10 11:06:46 -0800

    l

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-fprintf(stderr, &ch);
+fprintf(stderr, "and that piece of art is useful
+\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
