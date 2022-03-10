e63488cf8a2b8e41f3665480b77fcc116ddfb261
EdlawitG <eduedil30@gmail.com>
2022-03-10 05:33:17 -0800

    last commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- *
- * Return: Always 0 (Success)
+ * Output using puts
+ * Return: 0
  */
+
 int main(void)
 {
-    puts("\"programming is like buliding a multilingual puzzel");
-        return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
 
 }
