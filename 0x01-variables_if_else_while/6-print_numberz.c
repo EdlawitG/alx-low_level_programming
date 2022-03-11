b72f4a0d977ab9c5eb972186d919ac53dee589b7
EdlawitG <eduedil30@gmail.com>
2022-03-11 05:32:20 -0800

    i hate i love u

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * print num 
+ * Return: 0
+ */
+int main(void)
+{
+int num = 0;
+while (num < 10)
+{
+putchar(48 + num);
+num++;
+}
+putchar('\n');
+return (0);
+}
