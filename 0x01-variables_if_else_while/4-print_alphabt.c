07fc9b9d3bdb7a1d9c0f8ee9a4332ad057749ddc
EdlawitG <eduedil30@gmail.com>
2022-03-11 04:05:01 -0800

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * except q and e
+ * Return: 0
+ */
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+if (ch != 'e' && ch != 'q')
+{
+putchar(ch);
+}
+ch++;
+}
+putchar('\n');
+return (0);
+}
