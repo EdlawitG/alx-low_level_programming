f08f5d45344671dfe7aaafa8063be5f29267f1c1
EdlawitG <eduedil30@gmail.com>
2022-03-11 03:34:16 -0800

    lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * lower case
+ * Return: 0
+ */
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+putchar("\n");
+return (0);
+}
