95c0cfa20fefac0a5b03c018a83e8f37caa13089
EdlawitG <eduedil30@gmail.com>
2022-03-11 03:21:21 -0800

    wtf

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-/*more header goes there*/
-/* betty style doc for function goes there*/
-/*main - Entry point */
+/**more header goes there
+ *betty style doc for function goes there
+ *main - Entry point 
+ */
 int main(void)
 {
 int n, digit;
