db1fd4b52aa7284242de05bf4c28d4e3e8053904
EdlawitG <eduedil30@gmail.com>
2022-03-15 17:55:50 +0300

    is times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,16 +11,33 @@ void times_table(void)
 	int mult;
 	int pro;
 
-	for (num = 0; num < 10; num++)
-	{
-		for (mult = 0; mult < 10; mult++)
-		{
-			pro = num * mult;
-			_putchar(pro + '0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-		}
-		_putchar('\n');
-	}
+for (num = 0; num < 10; num++)
+{
+for (mult = 0; mult < 10; mult++)
+{
+pro = num * mult;
+if (mult == 0)
+{
+_putchar(pro + '0');
+}
+else if (pro >= 10)
+{
+_putchar(' ');
+_putchar(pro / 10 + '0');
+_putchar(pro % 10 + '0');
+}
+else
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(pro + '0');
+}
+
+if (mult != 9)
+{
+_putchar(',');
+}
+}
+_putchar('\n');
+}
 }
