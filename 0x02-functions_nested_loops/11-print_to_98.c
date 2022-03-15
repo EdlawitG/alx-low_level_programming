704532fb10d1beebb78c10afce78a50f7a7946f0
EdlawitG <eduedil30@gmail.com>
2022-03-15 18:10:13 +0300

    is printing upto 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,8 +6,8 @@
  * @n : integer is used
  * Return: Always 0.
  */
- void print_to_98(int n)
- {
+void print_to_98(int n)
+{
 while(n < 98)
 {
 printf("%i, ", n);
@@ -15,9 +15,9 @@ n++;
 }
 while(n > 98)
 {
-    printf("%i, ", n);
-    n--;
+printf("%i, ", n);
+n--;
 }
 printf("98");
 putchar('\n');
- }
+}
