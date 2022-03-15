950db78518394e33f8a5f447c465532eff206bb5
EdlawitG <eduedil30@gmail.com>
2022-03-15 12:20:35 +0300

    hello

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int num;
+	int mult;
+	int pro;
+
+	for (num = 0; num < 10; num++)
+	{
+		for (mult =0; mult < 10; mult++)
+		{
+			pro = num * mult;
+			_putchar(pro);
+			_putchar(',');
+			_putchar('  ');
+		}
+		_putchar('\n');
+	}
+}
+int main(void)
+{
+    times_table();
+    return (0);
+}
