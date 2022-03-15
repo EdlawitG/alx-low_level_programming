30a49d8340ef296fb796eac685ee7ae30a1fbee2
EdlawitG <eduedil30@gmail.com>
2022-03-15 17:15:00 +0300

    is positive

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,22 +7,22 @@
  */
 int print_sign (int n)
 {
-    if (n > 0)
-    {
-        return (1);
-        _putchar('+');
-        _putchar(1);
-    }
-    else if (n < 0)
-    {
-        return (-1);
-        _putchar('-');
-        _putchar(1);
-    }
-    else
-    {
-        return (0);
-        _putchar('0');
-        _putchar(0);
-    }
+if (n > 0)
+{
+return (1);
+_putchar('+');
+_putchar(1);
+}
+else if (n < 0)
+{
+return (-1);
+_putchar('-');
+_putchar(1);
+}
+else
+{
+return (0);
+_putchar('0');
+_putchar(0);
+}
 }
