f808e4db3193b5312f4e37f40d1731aa3970f796
EdlawitG <eduedil30@gmail.com>
2022-03-15 17:23:24 +0300

    is last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the code
+ * @n : integer is used
+ * Return: Always 0.
+ */
+int print_last_digit(int n)
+{
+	int x;
+
+	x = num % 10;
+	_putchar(x);
+	return (x);
+}
