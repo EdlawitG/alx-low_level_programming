86a2cd9bd09d5ad1ce5d744593a3375874b57e72
EdlawitG <eduedil30@gmail.com>
2022-03-17 06:17:07 -0700

    \fuck off

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main"
+
+/**
+ * mul - product of two numbers
+ * @a: first integer
+ * @b: second integer
+ * Return: the result of the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a*b);
+}
