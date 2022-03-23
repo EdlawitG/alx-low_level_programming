8c203d564db680ac611d63820d3daaa70e9d2f59
EdlawitG <eduedil30@gmail.com>
2022-03-23 13:40:35 +0300

    string concat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,20 +10,14 @@ char *_strcat(char *dest, char *src)
 {
     int c;
 
-    for (c = 0; c != '\0'; c++)
-    {
-        dest[c];
-    }
+    for (c = 0; dest[c] != '\0'; c++);
 
-    if (dest[c] == '\0')
-    {
         int i;
-        for (i = 0; i != '\0'; i++)
+        for (i = 0; src[i] != '\0'; i++)
         {
             dest[c] = src[i];
             c++;
         }
         dest[c] = '\0';
-    }
-    return (*dest);
+    return (dest);
 }
