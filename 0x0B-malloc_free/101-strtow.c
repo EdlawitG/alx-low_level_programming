e72e298d22540d823e4fa9b6b1cb5189ce33bd01
EdlawitG <eduedil30@gmail.com>
2022-04-04 11:45:52 -0700

    Holy

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,112 +1,111 @@
-#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * _strlen - find length of a string
- * @s: string
- * Return: int
+ * wordnos - counts no of words in a given string
+ * @str: pointer to the string
+ *
+ * Return: No. of words in the string (int)
  */
-
-
-int _strlen(char *s)
+int wordnos(char *str)
 {
-int size = 0;
-for (; s[size] != '\0'; size++)
-;
-return (size);
-}
+	int wordno, i, j;
 
-/**
- * *str_concat - concatenates two strings
- * @s1: string 1
- * @s2: string 2
- * Return: pointer
- */
-
-char *str_addChar (char *str, char c)
-{
-int size, i;
-char *m;
-
-size = _strlen(str);
-
-m = malloc((size + 1) * sizeof(char) + 1);
-if (m == 0)
-	return (0);
-
-for (i = 0; i <= size; i++)
-	m[i] = str[i];
-
-m[i + 1] = c;
-m[i + 2] = '\0';
-
-return (m);
-}
-
-
-/**
- * *nbr_spaces - return the number of occurent of a string
- * @s: string to check
- * Return: int
- */
-
-unsigned int nbr_spaces(char *s)
-{
-	int i, cmpt = 0;
-
-	for (i = 0; s[i + 1] != '\0'; i++)
+	wordno = 0;
+	i = 0;
+	while (*(str + i) != '\0')
 	{
-		if (s[i]  == ' ' && s[i + 1] != ' ')
-			cmpt++;
+		if (*(str + i) != 32 && *(str + i) != '\0')
+		{
+			j = i;
+			while (*(str + j) != 32 && *(str + j) != '\0')
+				j++;
+			wordno++;
+			i = j - 1;
+		}
+		i++;
 	}
-
-	return (cmpt + 1);
+	return (wordno);
 }
 
+/**
+ * cpystr - copies words in string to different elements of 2d array of strings
+ * @s: double pointer to a 2D array of strings
+ * @str: pointer to string whose words are to be copied
+ *
+ * Return: void
+ */
+void cpystr(char **s, char *str)
+{
+	int i, j, l, idx;
+
+	i = 0;
+	idx = 0;
+	while (*(str + i) != '\0')
+	{
+		if (*(str + i) != 32 && *(str + i) != '\0')
+		{
+			j = i;
+			l = 0;
+			while (*(str + j) != 32 && *(str + j) != '\0')
+			{
+				s[idx][l] = *(str + j);
+				l++;
+				j++;
+			}
+			s[idx][l] = '\0';
+			idx++;
+			i = j;
+		}
+		i++;
+	}
+}
 
 /**
-  *strtow - split a sentence into multiple words.
-  *@str: the string passed as argument.
-  *Return: tokens
-  */
+ * strtow - splits a string into words and stores the words in an array
+ * @str: pointer to string
+ *
+ * Return: double pointer to the array containing the words
+ */
 char **strtow(char *str)
 {
-int i;
-int spaces = nbr_spaces(str);
-char **tokens = NULL;//malloc(sizeof(char *) * (spaces));
-char *token;
-int checkingSpace = 0;
-int word = 0;
+	char **s;
+	int wordno, i, j, k, length, idx;
 
-if (!tokens)
-{
-	printf("Failed");
-	return (0);
-}
-	
-
-printf("looping");
-for (i = 0; str[i] != '\0'; i++)
-{
-	if (str[i] == ' ')
+	if (str == NULL || str[0] == '\0')
+		return (0);
+	wordno = wordnos(str);
+	s = (char **)malloc(sizeof(char *) * (wordno + 1));
+	if (s == 0 || wordno == 0)
+		return (0);
+	i = 0;
+	idx = 0;
+	while (*(str + i) != '\0')
 	{
-		if (checkingSpace == 0)
+		if (*(str + i) != 32 && *(str + i) != '\0')
 		{
-			word++;
-			checkingSpace = 1;
-		} 
+			j = i;
+			length = 0;
+			while (*(str + j) != 32 && *(str + j) != '\0')
+			{
+				length++;
+				printf("Length is %d\n", length);
+				j++;
+			}
+			*(s + idx) = (char *)malloc(sizeof(char) * (length + 1));
+			if (*(s + idx) == 0)
+			{
+				for (k = 0; k < idx; k++)
+					free(*(s + k));
+				free(s);
+				return (0);
+			}
+			idx++;
+			i = j - 1;
+			printf("value of i is %d \n", i);
+		}
+		i++;
 	}
-	else
-	{
-		printf("1");
-		token = tokens[word];
-		free(tokens[word]);
-		str_addChar(token, str[i]);
-		checkingSpace = 0;
-	}
-
-}
-
-tokens[i] = NULL;
-
-return (tokens);
+	cpystr(s, str);
+	return (s);
 }
