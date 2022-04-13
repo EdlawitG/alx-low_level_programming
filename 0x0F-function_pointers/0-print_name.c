1133a5dc8f25420dab8e65a6a9618364d632c37d
EdlawitG <eduedil30@gmail.com>
2022-04-13 02:40:27 -0700

    Girl like you

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,14 @@
 #include "function_pointers.h"
 
 /**
-* print_name - prints a name
-* @name: name to be printed
-* @f: callback function that prints the name*
-*/
+ * print_name - prints a name.
+ * @name: the name to be printed to stdout
+ * @f: a function pointer that determines the format of name
+ * Return: nothing
+ */
 void print_name(char *name, void (*f)(char *))
 {
-if(!f || !name)
-return;
-(*f)(name);
+	if (!name || !f)
+		return;
+	f(name);
 }
