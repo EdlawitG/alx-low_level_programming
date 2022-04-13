4159c0e9ac801a1d53c2b15a86453630906febcd
EdlawitG <eduedil30@gmail.com>
2022-04-13 01:53:11 -0700

    Papi

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - fuction to iterate given array
+ * @array: array parameter
+ * @size: size of the array
+ * @action: fuction pointer
+ * Return : nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	for(int i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
