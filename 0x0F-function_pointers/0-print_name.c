f75271ce6b13ddc45eccd56efe6676caf6f4a79b
EdlawitG <eduedil30@gmail.com>
2022-04-13 02:22:13 -0700

    Girl like you

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,13 @@
 #include "function_pointers.h"
+
 /**
- * print_name - to print name of passed argument
- * @name : character argument
- * @f: function pointer to void function
- * Return : nothing
+ * print_name - prints a name
+ * @name: string
+ * @f: function
  */
+
 void print_name(char *name, void (*f)(char *))
 {
+if (f != NULL)
 	f(name);
 }
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,15 +1,20 @@
 #include "function_pointers.h"
+
 /**
- * array_iterator - fuction to iterate given array
- * @array: array parameter
- * @size: size of the array
- * @action: fuction pointer
- * Return : nothing
+ * array_iterator - xecutes a function given as a parameter on each
+ * element of an array.
+ * @array: array of intege
+ * @size: size of array
+ * @action: function pointer
  */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	for(size_t i = 0; i < size; i++)
-	{
+size_t i;
+
+if (array != NULL && action != NULL)
+{
+	for (i = 0; i < size; i++)
 		action(array[i]);
-	}
+}
 }
